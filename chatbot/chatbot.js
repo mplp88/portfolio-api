98c3fb983bdcef6a6e90c171db168d41149166c1
Martín Ponce <martinp88@gmail.com>
2025-01-01 11:10:09 -0300

    remocion de console.log

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -4,7 +4,6 @@ const { init, receiveMessage } = require('./pusher')
 
 router.get('/', (req, res) => {
   try {
-    console.log('contactando al chatbot')
     const chatId = crypto.randomUUID()
     res.json({
       chatId
@@ -19,7 +18,6 @@ router.get('/', (req, res) => {
 router.get('/init/:chatid', (req, res) => {
   const chatId = req.params.chatid
   try {
-    console.log('iniciando el chatbot')
     init(chatId)
   } catch (error) {
     res.status(400).json({
@@ -33,6 +31,7 @@ router.post('/sendMessage/:chatId', (req, res) => {
     const chatId = req.params.chatId
     const message = req.body.message
     receiveMessage(chatId, message)
+    res.status(200).send()
   } catch (error) {
     res.status(400).send(error)
   }
